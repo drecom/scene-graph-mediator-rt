b3af2432a3c704cb6e7ffbbc44e1205349832783
dolow <velvet.nanashi@gmail.com>
2018-08-09 20:42:55 +0900

    build artifact

--- lib/lib-ts/importer/Pixi.d.ts
+++ lib/lib-ts/importer/Pixi.d.ts
@@ -4,6 +4,10 @@ import Importer from 'importer/Importer';
  * Pixi implementation of RuntimeMediator
  */
 export default class Pixi extends Importer {
+    onLoaderAdd: (node: Node, asset: {
+        url: string;
+        name: string;
+    }) => void;
     getInitiator(name: string): (node: Node) => any;
     hasInitiator(name: string): boolean;
     import(schema: SchemaJson, callback?: (root: any) => void): any;
